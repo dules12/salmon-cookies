cc6b9fadecfeccdbd2546f51bd7242e8f7c9afa2
dules12 <danielgeo@comcast.net>
2018-06-11 23:32:24 -0700

    Got a base goong on finding the random numbers and will work to complete the total information needed per bakery and expand to all the locations tomorrow

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
 'use strict';
 
-var storeLocations = 
+var pikeStore = {
+  minHours: 23,
+  maxHours: 65,
+  averageCookies: 6.3,
+}
+
+var pikeStats = ['pikeAverageCust', 'pikeStore.averageCookies'];
+
+function pikeAverageCust(min, max) {
+    var min = Math.floor(pikeStore.minHours);
+    var max = Math.ceil(pikeStore.maxHours);
+    var randomCustomers = (Math.random() * (max - min + 1)) + min;
+    return(randomCustomers);
+}
+
+
+
+
+pikeAverageCust();
+
+   
+      
+      
+
+
+
